658448cdcb3c1b2efa557c6efc6759681f4163c7
Willie Jin <willymjin@gmail.com>
2017-02-10 14:14:20 -0500

    added more to tex

--- data_WJ edited.py
+++ data_WJ edited.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForest
 from sklearn.ensemble import GradientBoostingRegressor as GradientBoosting
-
+from sklearn.cross_validation import KFold
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
+from sklearn.grid_search import RandomizedSearchCV
+#from rdkit import Chem
 """
 Read in train and test as Pandas DataFrames
 """
@@ -30,19 +35,39 @@ X_test = vals[test_idx:]
 print("Train features:", X_train.shape)
 print("Train gap:", Y_train.shape)
 print("Test features:", X_test.shape)
-
+del df_all
+del df_test
+del df_train
 def write_to_file(filename, predictions):
     with open(filename, "w") as f:
         f.write("Id,Prediction\n")
         for i,p in enumerate(predictions):
             f.write(str(i+1) + "," + str(p) + "\n")
 
-RF = RandomForestRegressor(n_jobs=-1,oob_score=True)
+#Dividing test data in 80/20 test train split
+X_train_2,X_val_2,Y_train_2,Y_val_2 = train_test_split(X_train, Y_train,test_size=0.2)
+#%%
+#Modified Original Code to get MSE
+LR = LinearRegression()
+LR.fit(X_train_2, Y_train_2)
+LR_pred = LR.predict(X_test)
+#%%
+#Random Forest
+RF = RandomForest(n_jobs=-1,oob_score=True)
 RF.fit(X_train, Y_train)
 #RF_pred = RF.predict(X_test)
 print("done loading")
 #%%
-RF = RandomForestRegressor(n_jobs=-1, n_estimators=30)
+#Tuning RF using exhaustive grid search
+rf_param_test = {'n_estimators':[10,30,50,70,90,110],'max_depth':['None',3,5,7,9]}
+RF_init = RandomForest(n_jobs=-1, max_features='auto')
+RF_search = GridSearchCV(estimator = RF_init, param_grid = rf_param_test,scoring='mean_squared_error',n_jobs=-1,iid=False)
+print("done with GridSearch")
+RF_search.fit(X_train, Y_train)
+print("done with fit")
+RF_search.best_params_
+#%%
+RF = RandomForest(n_jobs=-1, n_estimators=30)
 RF.fit(X_train, Y_train)
 RF_pred = RF.predict(X_test)
 write_to_file("rf4_added.csv",RF_pred)
@@ -54,3 +79,4 @@ ypred = GB.predict(X_test)
 print("done fitting")
 write_to_file("test2gb_added.csv", ypred)
 print("done with gb")
+
